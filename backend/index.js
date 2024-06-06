013982b338785e74d88f14008657ac7d598d6aff
Adolfopgv <adolfopgv@gmail.com>
2024-06-06 10:22:20 +0200

    pruebas deploy index.js cambiado

--- backend/index.js
+++ backend/index.js
@@ -1,17 +1,20 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config({ path: __dirname + "/.env" });
+}
 const cors = require("cors");
 const db = require("./helpers/db"); // Database connection
 const cookieParser = require("cookie-parser");
 const app = express();
 app.disable("x-powered-by");
 const bodyParser = require("body-parser");
+const path = require('path');
 
 app.use(
   cors({
     credentials: true,
-    origin: ["http://localhost:5173"],
-    methods: ["POST", "GET", "DELETE"]
+    origin: [process.env.NODE_ENV !== "production" ? "http://localhost:5173" : process.env.BASE_URL],
+    methods: ["POST", "GET", "DELETE"],
   })
 );
 
@@ -28,7 +31,7 @@ app.use("/", require("./routes/cartRoutes"));
 app.use("/", require("./routes/userRoutes"));
 app.use("/", require("./routes/orderRoutes"));
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 app.listen(port, () =>
   console.log(`Server is running on http://localhost:${port}`)
 );
